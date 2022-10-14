d0b8ed849c98ee722e7bd14379b83c379bb4c82f
kamranBaloch1 <kamranbalochhh18@gmail.com>
2022-10-14 21:52:12 +0100

    first commit

---
+++ src/utils/fetchData.js
@@ -0,0 +1,22 @@
+export const exerciseOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+    'X-RapidAPI-Key': 'fd814a02aemsh3a62c5fc93380e9p14679cjsna80931178e1c',
+  },
+};
+
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+    'X-RapidAPI-Key': 'fd814a02aemsh3a62c5fc93380e9p14679cjsna80931178e1c',
+  },
+};
+
+export const fetchData = async (url, options) => {
+  const res = await fetch(url, options);
+  const data = await res.json();
+
+  return data;
+};
